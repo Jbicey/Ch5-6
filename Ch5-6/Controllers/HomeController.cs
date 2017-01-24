1d6f2ffdd53ea317128bd4fb1fa9d889d0e97b3f
Jared <jared.bice@gmail.com>
2017-01-24 14:50:51 -0700

    Commit from 3:00 PM. Left off @ "Using Conditional Statements"

--- Ch5-6/Controllers/HomeController.cs
+++ Ch5-6/Controllers/HomeController.cs
@@ -21,5 +21,18 @@ namespace Ch5_6.Controllers
         {
             return View(myProduct);
         }
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
     }
 }
